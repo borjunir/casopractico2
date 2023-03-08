a90073ea2d5358ed1460ece316787248c80a1b43
borja.rodriguez@tecnalia.com <borja.rodriguez@tecnalia.com>
2023-03-08 10:26:26 +0100

    Construccion correcta de tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,18 +1,18 @@
 #Configuracion terraform Azure provider
 terraform {
-  requiered_providers {
+  required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
-      version = "3.46.0"
+      soursource = "hashicorp/azurerm"
+      versversion = "3.46.0"
     }
   }
   required_version = ">= 1.1.0"
 }
-#Configuracion Azure Provider. Deletion de recursos para las pruebas
+#Azure Provider con delete resources activado
 provider "azurerm" {
   features {
     resource_group {
-	prevent_deletion_if_contains_resources = false
+      prevent_deletion_if_contains_resources = false
     }
   }
 }
--- terraform/vars.tf
+++ terraform/vars.tf
@@ -1,1 +1,18 @@
 #Variables
+variable "location" {
+  type = string
+  description = "Region de Azure donde crearemos la infraestructura"
+  default = "<YOUR REGION>"
+}
+
+variable "public_key_path" {
+  type = string
+  description = "Path clave publica acceso instancias"
+  default = "~/.ssh/id_rsa.pub"
+}
+
+variable "ssh_user" {
+  type = string
+  description = "User SSH"
+  default = "<SSH USER>"
+}
