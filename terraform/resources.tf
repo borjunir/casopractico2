913cb4ebd573597fe3fcfb310b9effbe220945fa
borja.rodriguez@tecnalia.com <borja.rodriguez@tecnalia.com>
2023-03-08 19:17:39 +0100

    Primeras pruebas terraform

--- terraform/resources.tf
+++ terraform/resources.tf
@@ -1,1 +1,4 @@
-#Recursos
+resource "azurerm_resource_group" "rg" {
+  name = var.resource_group_name
+  location = var.location_name
+}
--- terraform/vars.tf
+++ terraform/vars.tf
@@ -1,18 +1,34 @@
-#Variables
-variable "location" {
+/// AZURE ///
+variable "resource_group_name" {
+  description = "ResourceGroup creado por Terraform"
+  default = "rg-ByTerraform"
+}
+variable "location_name" {
   type = string
   description = "Region de Azure donde crearemos la infraestructura"
-  default = "<YOUR REGION>"
+  default = "uksouth"
 }
-
+/// Credenciales ///
 variable "public_key_path" {
   type = string
   description = "Path clave publica acceso instancias"
   default = "~/.ssh/id_rsa.pub"
 }
-
 variable "ssh_user" {
   type = string
   description = "User SSH"
   default = "<SSH USER>"
+}
+/// Descripcion Imagenes ///
+variable "image_name" {
+  description = "Nombre de la imagen a utilizar"
+  default = "centos-8-stream-free"
+}
+variable "image_version" {
+  description = "Version de la imagen a utilizar"
+  default = "latest"
+}
+variable "image_publisher" {
+  description = "MarketPlace Proveedor imagen"
+  default = "Cognosys"
 }
