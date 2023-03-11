f8978e722a5c82ea4b7d0c32ce640385d137eefb
borja.rodriguez@tecnalia.com <borja.rodriguez@tecnalia.com>
2023-03-11 17:19:09 +0100

    Ip Publica a estatica

--- terraform/resources.tf
+++ terraform/resources.tf
@@ -77,7 +77,7 @@ resource "azurerm_public_ip" "vippublic" {
   name = "vm-ippublic"
   location = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  allocation_method = "Dynamic"
+  allocation_method = "Static"
   sku = "Standard"
   tags = var.tag_resources
 }
