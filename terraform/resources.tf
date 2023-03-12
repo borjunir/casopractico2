f824c872b756f13f44250816b71c25c5753d77cb
borja.rodriguez@tecnalia.com <borja.rodriguez@tecnalia.com>
2023-03-12 11:39:06 +0100

    Cambio para poner reglas de NSG

--- terraform/resources.tf
+++ terraform/resources.tf
@@ -88,33 +88,31 @@ resource "azurerm_network_security_group" "nsg" {
   location = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   tags = var.tag_resources
+
+  security_rule {
+    name = "SSH"
+    priority = 1001
+    direction = "Inbound"
+    access = "Allow"
+    protocol = "Tcp"
+    source_port_range = "*"
+    destination_port_range = "22"
+    source_address_prefix = "*"
+    destination_address_prefix = "*"
+  }
+  security_rule {
+    name = "Ingress"
+    priority = 1002
+    direction = "Inbound"
+    access = "Allow"
+    protocol = "Tcp"
+    source_port_range = "*"
+    destination_port_range = "30000-32767"
+    source_address_prefix = "*"
+    destination_address_prefix = "*"
+  }
 }
-resource "azurerm_network_security_rule" "ssh" {
-  name = "SSH"
-  priority = 1001
-  direction = "Inbound"
-  access = "Allow"
-  protocol = "Tcp"
-  source_port_range = "*"
-  destination_port_range = "22"
-  source_address_prefix = "*"
-  destination_address_prefix = "*"
-  resource_group_name = azurerm_resource_group.rg.name
-  network_security_group_name = azurerm_network_security_group.nsg.name
-}
-resource "azurerm_network_security_rule" "ingress" {
-  name = "Ingress"
-  priority = 1002
-  direction = "Inbound"
-  access = "Allow"
-  protocol = "Tcp"
-  source_port_range = "*"
-  destination_port_range = "30000-32767"
-  source_address_prefix = "*"
-  destination_address_prefix = "*"
-  resource_group_name = azurerm_resource_group.rg.name
-  network_security_group_name = azurerm_network_security_group.nsg.name
-}
+
 resource "azurerm_network_interface_security_group_association" "nsgassociation" {
   network_interface_id = azurerm_network_interface.NIC.id
   network_security_group_id = azurerm_network_security_group.nsg.id
