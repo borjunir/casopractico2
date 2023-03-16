3dfb30fd89e0ded5bfe9a775be8ebf68f3bc598e
borja.rodriguez@tecnalia.com <borja.rodriguez@tecnalia.com>
2023-03-16 19:03:30 +0100

    SKU changed

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -67,11 +67,11 @@ variable "os_image" {
     sku       = string
   })
   default = {
-    offer     = "0001-com-ubuntu-minimal-focal"
+    offer     = "0001-com-ubuntu-minimal-jammy"
     name      = "Ubuntu-20.04"
     version   = "latest"
     publisher = "Canonical"
-    sku       = "20_04-lts-gen2"
+    sku       = "minimal-22_04-lts-gen2"
   }
 }
 
