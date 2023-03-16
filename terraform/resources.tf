289406db29fef9137a1ff5461b8c988197ece784
borja.rodriguez@tecnalia.com <borja.rodriguez@tecnalia.com>
2023-03-16 15:01:14 +0100

    Modificacione de rango para acceso desde fuera Añadidas reglas

--- terraform/resources.tf
+++ terraform/resources.tf
@@ -139,18 +139,29 @@ resource "azurerm_network_security_group" "nsg" {
     protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_range     = "22"
-    source_address_prefix      = "85.55.192.161"
+    source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
   security_rule {
-    name                       = "Ingress"
+    name                       = "HTTPS"
     priority                   = 1002
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "Tcp"
     source_port_range          = "*"
-    destination_port_range     = "30000-32767"
+    destination_port_range     = "443"
     source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+  security_rule {
+    name = "HTTP"
+    priority = 1003
+    direction = "Inbound"
+    access = "Allow"
+    protocol = "Tcp"
+    source_port_range = "*"
+    destination_port_range = "80"
+    source_address_prefix = "*"
     destination_address_prefix = "*"
   }
 }
