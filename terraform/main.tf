49ef3148f85de0cfb5f97058a937786fea4bbad3
borjunir <borja.rodriguezlopezd077@comunidadunir.net>
2023-03-20 14:23:39 +0100

    modificaciones varias

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,8 +11,6 @@ terraform {
 #Azure Provider con delete resources activado
 provider "azurerm" {
   features {
-    resource_group {
-      prevent_deletion_if_contains_resources = false
     }
   }
 }
